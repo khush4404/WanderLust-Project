3be6cc61d41c5b257b9feebad542ca8897c84c23
khush <vagadiyakhush@gmail.com>
2024-12-26 11:49:52 +0530

    after some changes final commit

--- init/index.js
+++ init/index.js
@@ -15,7 +15,7 @@ main().then(()=>{
 
 const initDB = async () =>{
     await Listing.deleteMany({});
-    initData.data = initData.data.map((obj)=> ({...obj, owner : '67690477045f24155e0155b6'}))
+    initData.data = initData.data.map((obj)=> ({...obj, owner : '676cf09b78444010d2467c2f'}))
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
